063f24e08cc8aba08c3cd862536c5bdc33d17787
tanmay2810 <t.kabade07@gmail.com>
2023-05-02 13:25:26 +0530

    update

--- async fuctions/script.js
+++ async fuctions/script.js
@@ -2,6 +2,7 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
+const imageContainer = document.querySelector(".images")
 // https://restcountries.com/v3.1/name/
 
 const errorHandling = (message) => {
@@ -158,10 +159,11 @@ const getJson = (url, errorMessage = "Something went wrong") => {
 };
 
 const getCountryData = function (country) {
-  getJson(`https://restcountries.com/v3.1/name/${country}`, "Country not found").then((data) => {
+  getJson(`https://restcountries.com/v3.1/name/${country}`, "Country not found")
+    .then((data) => {
       renderCountry(data);
       const neighbours = data[0].borders[0];
-      if (!neighbours) throw new Error('No neighbour')
+      if (!neighbours) throw new Error("No neighbour");
 
       return getJson(
         `https://restcountries.com/v3.1/alpha/${neighbours}`,
@@ -183,20 +185,152 @@ btn.addEventListener("click", () => {
 });
 // getCountryData("123");
 
+// challenge 1
+
+// let coords = []
+// navigator.geolocation.getCurrentPosition((position) =>{
+//   coords.push(position.coords.latitude)
+//   coords.push(position.coords.longitude)
+// },() =>{
+//   console.log('could not get ur location')
+// })
+
+// console.log(coords)
+
+// fetch(`https://geocode.xyz/${coords[0]},${coords[1]}?geoit=json`).then( (response) =>{
+//   console.log(response)
+//   if(!response.ok) throw new Error('API not working')
+//   response.json()
+// }).then((data) => console.log(data.city)).catch(error => { console.error(error) }).finally( () => console.log('finally'))
+
+// const result =  fetch(`https://restcountries.com/v3.1/name/${'usa'}`).then((response) => {
+//   if (!response.ok) throw new Error(`${response.status}`);
+//   return response.json();
+// });
+// console.log(result.then(data => console.log(data)),'log')
+
+// -----------------------------------------------------------------
+// building own promises
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log("lottery draw is happening");
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve("U won");
+    } else reject(new Error("U lost"));
+  }, 2000);
+});
+
+// prettier-ignore
+// lotteryPromise.then(data => console.log(data)).catch(error => console.error(error))
+
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(1)
+//   .then(() => {
+//     console.log("1 sec delay");
+//     return wait(2);
+//   })
+//   .then(() => console.log("2 sec delay"));
+
+// // navigator.geolocation.getCurrentPosition(
+// //   (position) => console.log(position),
+// //   (error) => console.log(error)
+// // );
+
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject) {
+//     // both works
+//     // navigator.geolocation.getCurrentPosition( resolve(position), reject('location not found') )
+//     navigator.geolocation.getCurrentPosition(
+//       resolve,
+//       reject
+//     );
+//   });
+// };
+// getPosition()
+//   .then((position) => {
+//     const { latitude: lat, longitude: lng } = position.coords;
+//     console.log(lat, lng);
+//   })
+//   .catch(() => console.log('error'));
+
+// getPosition()
+//   .then((position) => {
+//     const { latitude: lat, longitude: lng } = position.coords;
+
+//     return fetch(`https://geocode.xyz/${coords[0]},${coords[1]}?geoit=json`);
+//   })
+//   .then((resposne) => resposne.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
+// ---------------------------------------------------------------------
+// coding chalange 2
+
+// let currentImg
+// const createImage = function(url){
+//   return new Promise(function(resolve,reject){
+//     const img = document.createElement('img')
+//     console.log(url)
+//     img.src = url
+
+//     img.addEventListener('load', function(){
+//       imageContainer.append(img)
+//       resolve(img)
+//     })
+
+//     img.addEventListener('error', function(){
+//       reject(new Error('img not found'))
+//     })
+//   })
+// }
+// createImage('img/img-1.jpg').then(img => {
+//   currentImg = img
+//   console.log('success 1')
+//   return wait(2)
+// }).then(
+//   () =>{
+//     currentImg.style.display = "none"
+//     return createImage('img/img-2.jpg')
+//   }
+// ).then(img => {
+//   currentImg = img
+//   console.log('success 2')
+//   return wait(2)
+// }).then(
+//   () =>{
+//     // currentImg.style.display = "none"
+//   }
+// )
+// .catch( error => console.log(error))
 
 
-let coords = []
-navigator.geolocation.getCurrentPosition((position) =>{
-  coords.push(position.coords.latitude)
-  coords.push(position.coords.longitude)
-},() =>{
-  console.log('could not get ur location')
-})
+// async await
 
-console.log(coords)
 
-fetch(`https://geocode.xyz/${coords[0]},${coords[1]}?geoit=json`).then( (response) =>{
-  console.log(response)
-  if(!response.ok) throw new Error('API not working')
-  response.json()
-}).then((data) => console.log(data.city)).catch(error => { console.error(error) }).finally( () => console.log('finally'))
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // both works
+    // navigator.geolocation.getCurrentPosition( resolve(position), reject('location not found') )
+    navigator.geolocation.getCurrentPosition(
+      resolve,
+      reject
+    );
+  });
+};
+
+const wherAmI = async function(country){
+  const position =  await getPosition()
+  const { latitude: lat, longitude: lng } = position.coords;
+  console.log(lat,lng)
+  const res = await fetch(`https://restcountries.com/v3.1/name/${country}`)
+  const data = res.json()
+  return data
+}
+
+wherAmI('bharat').then( data => renderCountry(data) )
