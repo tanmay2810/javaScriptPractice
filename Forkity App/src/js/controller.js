316ca7152efd38f45504b32a2dd3532074179675
tanmay2810 <t.kabade07@gmail.com>
2023-05-21 23:32:01 +0530

    forkyt app

--- Forkity App/src/js/controller.js
+++ Forkity App/src/js/controller.js
@@ -1,4 +1,10 @@
-const recipeContainer = document.querySelector('.recipe');
+
+import "core-js";
+import "regenerator-runtime";
+import { state, loadRecipe } from "./model";
+import recipeView from "./views/recipeView";
+
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -7,6 +13,32 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
+
+console.log("Hello");
+
+
+
+const controllerRecipe = async function (id) {
+  try {
+    recipeView.renderSpinner
+    // const id = window.location.hash.slice(1)
+    // console.log(id)
+    if (!id) return
+
+    // loading recipe
+    await loadRecipe(id)
+    
+    // Rendering recipe
+    recipeView.render(state.recipe)
+  } catch (error) {
+    alert(error);
+  }
+};
+
+['hashchange','load'].forEach(ev => window.addEventListener(ev, controllerRecipe('5ed6604591c37cdc054bcb34')))
+
+// window.addEventListener('hashchange', showRecipe('5ed6604591c37cdc054bcb34'))
+// window.addEventListener('load', showRecipe('5ed6604591c37cdc054bcb34'))
 
 // https://forkify-api.herokuapp.com/v2
 
